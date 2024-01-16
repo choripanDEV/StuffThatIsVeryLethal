3254487e2c802c6b1a33d5e4aeab45b9ea8393e1
choripanDEV <choripan.dev@gmail.com>
2024-01-16 15:03:14 +0100

    Start of the project

---
+++ Plugin.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx;
+
+namespace LethalCompanyTemplate
+{
+    [BepInPlugin("org.choripandev.plugins.stuffthatisverylethal", "Stuff that is very Lethal", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {"org.choripandev.plugins.stuffthatisverylethal"} is loaded!");
+        }
+    }
+}
---
+++ obj/Debug/netstandard2.1/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace StuffThatIsVeryLethal
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "StuffThatIsVeryLethal";
+        public const string PLUGIN_NAME = "StuffThatIsVeryLethal";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
